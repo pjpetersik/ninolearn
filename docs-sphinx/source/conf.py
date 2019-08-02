ce1b991a5d8c37d5b3ad587e01300b60de2e72c3
paul <paul.petersik@gmail.com>
2019-08-02 11:05:26 +0200

    test view on github

--- docs-sphinx/source/conf.py
+++ docs-sphinx/source/conf.py
@@ -94,6 +94,13 @@ html_theme = 'sphinx_rtd_theme'# 'sphinxdoc'
 #
 html_theme_options = {'logo_only': True}
 
+html_context = {
+"display_github": True, # Add 'Edit on Github' link instead of 'View page source'
+"last_updated": True,
+"commit": False,
+}
+
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
